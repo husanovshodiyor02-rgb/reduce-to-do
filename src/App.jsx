31a7e160636d54177f3f4e8ee18c7cfd12266dc7
HusanboyevShodiyor <husanovshodiyor02@gmail.com>
2025-11-30 13:44:18 -0930

    The end

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
       <div className="min-h-screen bg-gradient-to-br from-blue-50 to-blue-100 py-10 px-4">
         <div className="max-w-2xl mx-auto">
           <h1 className="text-4xl font-bold text-center text-blue-700 drop-shadow mb-5">
-            ✨ Zamonaviy Todo List
+            Zamonaviy Todo List
           </h1>
 
           <TodoForm />
